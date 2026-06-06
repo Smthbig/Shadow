a325a0bbb36a3f206e79840bb7b34e4f4517ae30
algospider <mohanrajauria@gmail.com>
2026-06-07 01:52:13 +0530

    fix: added import

--- app/src/main/java/com/smthbig/shadow/launcher/home/IntentBarView.java
+++ app/src/main/java/com/smthbig/shadow/launcher/home/IntentBarView.java
@@ -1,6 +1,7 @@
 package com.smthbig.shadow.launcher.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.text.Editable;
